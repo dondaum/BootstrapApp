117505fae8d362b5d3531503715f58c9c61d5cd4
dondaum <sebastian.daum89@gmail.com>
2017-10-08 19:41:04 +0200

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  get 'help', to: 'welcome#help'
+
+  get 'contact', to: 'welcome#contact'
+
+  root to: 'welcome#home'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
